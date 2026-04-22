ddd9047b4b530e2ea5cdc75817c0a5d60286a748
Hedy1234567 <hedimedd10@gmail.com>
2026-04-22 15:56:25 +0100

    Ajout sidebar + amélioration UI avec fragments (navbar, footer, sidebar)

---
+++ src/main/java/com/fst/cabinet/dto/MedecinDto.java
@@ -0,0 +1,80 @@
+package com.fst.cabinet.dto;
+
+public class MedecinDto {
+
+    private Long id;
+    private String nom;
+    private String prenom;
+    private String specialite;
+    private String numeroOrdre;
+    private String telephone;
+    private String email;
+    private boolean actif;
+
+    public MedecinDto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getSpecialite() {
+        return specialite;
+    }
+
+    public void setSpecialite(String specialite) {
+        this.specialite = specialite;
+    }
+
+    public String getNumeroOrdre() {
+        return numeroOrdre;
+    }
+
+    public void setNumeroOrdre(String numeroOrdre) {
+        this.numeroOrdre = numeroOrdre;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isActif() {
+        return actif;
+    }
+
+    public void setActif(boolean actif) {
+        this.actif = actif;
+    }
+}
---
+++ src/main/java/com/fst/cabinet/dto/PatientDto.java
@@ -0,0 +1,82 @@
+package com.fst.cabinet.dto;
+
+import java.time.LocalDate;
+
+public class PatientDto {
+
+    private Long id;
+    private String cin;
+    private String nom;
+    private String prenom;
+    private LocalDate dateNaissance;
+    private String telephone;
+    private String email;
+    private String antecedents;
+
+    public PatientDto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCin() {
+        return cin;
+    }
+
+    public void setCin(String cin) {
+        this.cin = cin;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public LocalDate getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(LocalDate dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAntecedents() {
+        return antecedents;
+    }
+
+    public void setAntecedents(String antecedents) {
+        this.antecedents = antecedents;
+    }
+}
