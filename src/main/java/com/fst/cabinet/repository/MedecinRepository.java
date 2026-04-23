41599d2f148f88bce19e5a29c6fca375c76bbc31
Fahd <fehdout@gmail.com>
2026-04-23 12:10:19 +0100

    Final state of feature-fahd

--- src/main/java/com/fst/cabinet/repository/MedecinRepository.java
+++ src/main/java/com/fst/cabinet/repository/MedecinRepository.java
@@ -1,25 +1,21 @@
 package com.fst.cabinet.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.fst.cabinet.entity.Medecin;
-
-import java.util.List;
 
 public interface MedecinRepository extends JpaRepository<Medecin, Long> {
 
-    boolean existsByNumeroOrdre(String numeroOrdre);
-
     Optional<Medecin> findByNumeroOrdre(String numeroOrdre);
 
-    List<Medecin> findByNomContainingIgnoreCaseOrPrenomContainingIgnoreCaseOrSpecialiteContainingIgnoreCaseOrNumeroOrdreContainingIgnoreCase(
-    String nom,
-    String prenom,
-    String specialite,
-    String numeroOrdre
-);
+    boolean existsByNumeroOrdre(String numeroOrdre);
 
-    
+    List<Medecin> findByNomContainingIgnoreCaseOrPrenomContainingIgnoreCaseOrSpecialiteContainingIgnoreCaseOrNumeroOrdreContainingIgnoreCase(
+        String nom,
+        String prenom,
+        String specialite,
+        String numeroOrdre
+    );
 }
